55b864a2fe1aa576b33ec5b0ee38d9bb2f408e34
yum14 <yumurata1214@gmail.com>
2024-05-01 15:22:55 +0900

    :sparkles: Open AIリクエスト実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+
+import * as dotenv from "dotenv";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -19,3 +21,28 @@ export const helloWorld = onRequest(
         logger.info("Hello logs!", { structuredData: true });
         response.send("Hello from Firebase!");
     });
+
+import { OpenAI, ClientOptions } from "openai";
+
+dotenv.config();
+
+export const conversation = onRequest(
+  { region: "asia-northeast1", cors: true },
+  async (request, response) => {
+
+    const clientOptions: ClientOptions = {
+      apiKey: process.env.OPENAI_API_KEY
+    };
+
+    const openai = new OpenAI(clientOptions);
+
+    const openaiResponse = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-16k",
+      messages: [{ role: "user", content: "こんにちは" }]
+    });
+
+    const answer = openaiResponse.choices[0].message?.content;
+    logger.debug(answer);
+    response.send(answer);
+  }  
+);
